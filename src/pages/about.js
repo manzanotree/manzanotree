64258e06ffe161739f6423bc703b9cd93b48f1c9
Mike <manzano.michael@gmail.com>
2023-02-05 21:49:12 -0500

    about component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import About from '../components/about'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About">
-      <p>About Component Placeholder</p>
+      <About />
     </Layout>
   )
 }
