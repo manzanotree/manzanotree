6c88e15092fb8efa175085f46d8cd565fb93a2a5
manzanotree <manzano.michael@gmail.com>
2025-03-11 12:21:22 +0000

    add music section

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,21 +8,27 @@ import {
   navItem,
   contentWrapper,
   content,
+  contentMusic,
   footer
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+  let contentClass = content
+  if (pageTitle === 'Music') {
+    contentClass = contentMusic
+  }
   return (
     <div className={container}>
       <nav className={navWrapper}>
         <ul className={nav}>
           <li><Link className={navItem} to="/">manzanotree</Link></li>
           <li><Link className={navItem} to="/work">work</Link></li>
+          <li><Link className={navItem} to="/music">music</Link></li>
           <li><Link className={navItem} to="/about">about</Link></li>
         </ul>
       </nav>
       <div className={contentWrapper}>
-        <div className={content}>{children}</div>
+        <div className={contentClass}>{children}</div>
       </div>
       <footer className={footer}>
         Copyright {new Date().getFullYear()}
---
+++ src/components/music.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import {
+  container,
+  playlist,
+  track
+} from "./music.module.css"
+
+const Music = () => {
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const getTracks = async () => {
+      const response = await fetch(`https://api-v2.hearthis.at/kuya-mustard?type=tracks`)
+      const data = await response.json()
+      setTracks(data)
+    }
+
+    getTracks()
+  }, [])
+
+  const Music = () => {
+    if (tracks.length <= 0) return
+    return tracks.map((item, index) => {
+      return (
+        <li key={index} className={track}>
+          <iframe 
+            id={`hearthis_at_track_${item.id}`} 
+            width="100%" 
+            height="130" 
+            src={`https://app.hearthis.at/embed/${item.id}/transparent/?hcolor=&color=&style=2&block_size=5&block_space=1&background=0&waveform=0&cover=0&autoplay=0&css=`} 
+            frameborder="0" 
+            allowtransparency/> 
+        </li>
+      )
+    })
+  }
+
+  return (
+    <div className={container}>
+      <ul className={playlist}>
+        <Music />
+      </ul>
+    </div>
+  )
+}
+
+export default Music
